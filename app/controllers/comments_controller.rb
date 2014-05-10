dbc48d3073815dad2e5fd7c1fdcda1442328885a
xiaofu <mynameisfushengyang@gmail.com>
2014-05-10 09:44:57 +0800

    comments-controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,2 @@
-class CommentsController < AppliCationController
-    before_action :user_signed_in, only: [:create, :destroy]
-
-  def create
-  	 @comment = current_user.comments.build(comment_params)
-    if @comment.save
-      flash[:success] = "Comment created!"
-      redirect_to root_url
-    else
-      render 'static_pages/home'
-    end
-  end
-
-  def destroy
-  end
-
-  def index
-  end
-
-  private
-
-    def comment_params
-      params.require(:comment).permit(:content,:video_id,:user_id)
-    end
+class CommentsController < ApplicationController
 end

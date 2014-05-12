f38b7e3f68d36101f39278772c3a88c1e95a305a
xiaofu <mynameisfushengyang@gmail.com>
2014-05-12 22:23:24 +0800

    area-fujian

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,11 +8,11 @@ class StaticPagesController < ApplicationController
   	@food_sichuan = Food.limit(4).where(area:"sichuan")
   	@food_guangzhou = Food.limit(4).where(area:"guangzhou")
   	@food_shanxi = Food.limit(4).where(area:"shanxi")
-  	@food_aihui = Food.limit(4).where(area:"fujian")
+  	@food_fujian = Food.limit(4).where(area:"fujian")
   	@food_xianggang = Food.limit(4).where(area:"xianggang")
   	@food_beijing = Food.limit(4).where(area:"beijing")
   	
-  	@all_area_foods = [@food_guizhou,@food_beijing,@food_xianggang,@food_aihui,@food_shanxi,@food_guangzhou,@food_sichuan]
+  	@all_area_foods = [@food_guizhou,@food_beijing,@food_xianggang,@food_fujian,@food_shanxi,@food_guangzhou,@food_sichuan]
   end
 
   def help
